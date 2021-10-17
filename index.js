d116a726438c37866ce02e8a5cc982c11d7b31f7
Kaylany Paulus <kaylanysilvapaulus@gmail.com>
2021-10-17 06:02:53 -0300

    finalizado

---
+++ index.js
@@ -0,0 +1,5 @@
+let nome = window.prompt("Seu nome: ");
+let sobrenome = window.prompt("Seu sobrenome: ");
+
+window.alert(`Nome completo: ${nome} ${sobrenome}`);
+window.alert(`Nome de Catálogo: ${sobrenome.toUpperCase()}, ${nome}`);
